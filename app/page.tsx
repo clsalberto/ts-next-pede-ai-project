0aaf181499b71cc2012f45c5b9b1dac112fcc9be
Carlos Alberto <clsalberto@outlook.com>
2023-10-27 09:57:22 -0300

    chore: beginning of the project

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <p className="text-md font-medium text-secondary-foreground">
+        Bem-vindo ao
+      </p>
+      <h1 className="text-4xl font-bold text-primary">Pede AI!</h1>
+    </main>
+  )
+}
