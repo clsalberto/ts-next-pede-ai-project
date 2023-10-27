e24683e27e1e73d09b3867f18843ccae4fd0a2ab
Carlos Alberto <clsalberto@outlook.com>
2023-10-27 10:26:20 -0300

    chore: added changes to deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from 'next'
-import { Nunito as FontSans } from "next/font/google"
+import { Nunito as FontSans } from 'next/font/google'
 import { cn } from '~/lib/utils'
 import '~/styles/globals.css'
 
 export const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
+  subsets: ['latin'],
+  variable: '--font-sans',
 })
 
 export const metadata: Metadata = {
@@ -17,16 +17,18 @@ interface RootLayoutProps {
   children: React.ReactNode
 }
 
-export default function RootLayout({
-  children,
-}: RootLayoutProps) {
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
-      <body className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}>{children}</body>
+      <body
+        className={cn(
+          'min-h-screen bg-background font-sans antialiased',
+          fontSans.variable,
+        )}
+      >
+        {children}
+      </body>
     </html>
   )
 }
